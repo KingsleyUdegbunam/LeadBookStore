da2c0b99f3447047edbe035a30af408355884a06
kingsley <kingsleykay26@gmail.com>
2026-03-15 23:06:28 +0100

    Added cart and cart item badge display to Header component

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,17 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { searchIcon } from "./SearchIcon";
+// import { cartPrimary } from "../data/cart";
+import "./Header.css";
 
 export function Header() {
   const menuRef = useRef(null);
   const menuOpenRef = useRef(null);
   const menuCloseRef = useRef(null);
 
-  const toggleMenu = () => {
-    console.log(menuRef.current);
-    console.log("yes");
-    console.log(menuCloseRef.current);
-    console.log(menuOpenRef.current);
+  const [cart, setCart] = useState([]);
 
+  const toggleMenu = () => {
     menuRef.current.classList.toggle("view-menu");
     menuOpenRef.current.classList.toggle("close-menu-icon");
     document.body.classList.toggle("lock-scroll");
@@ -19,29 +19,19 @@ export function Header() {
   };
 
   const cartIcon = () => (
-    <svg
-      className="icon cart-icon"
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      viewBox="0 0 16 16"
-    >
-      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-    </svg>
-  );
-
-  const searchIcon = () => (
-    <svg
-      className="icon search-icon"
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      viewBox="0 0 16 16"
-    >
-      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-    </svg>
+    <div className="cart">
+      <span className="cart-item-number">{cart.length}</span>
+      <svg
+        className="icon cart-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+      </svg>
+    </div>
   );
 
   const menuIcon = () => (

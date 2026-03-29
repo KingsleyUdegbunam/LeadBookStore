a52e16a2fca7958a3cf3678953b2fefbb3263cd4
kingsley <kingsleykay26@gmail.com>
2026-03-29 20:31:30 +0100

    refactor(app): lift cart quantity updater to App.jsx

--- src/component/BookGrid.jsx
+++ src/component/BookGrid.jsx
@@ -1,43 +1,16 @@
 import { BookCard } from "./BookCard";
 import "./BookGrid.css";
 
-export function BookGrid({ books, setCart, cart }) {
-  const addToCart = (book) => {
-    const inCart = cart.find((cartItem) => cartItem.id === book.id);
-
-    if (inCart) {
-      setCart((prev) =>
-        prev.map((item) =>
-          item.id === book.id
-            ? {
-                ...item,
-                quantity: item.quantity + 1,
-                totalPrice: item.basePrice * (item.quantity + 1),
-              }
-            : item,
-        ),
-      );
-    } else {
-      setCart((prev) => [
-        ...prev,
-        {
-          id: book.id,
-          quantity: 1,
-          basePrice: book.price.paperback,
-          totalPrice: book.price.paperback,
-        },
-      ]);
-    }
-  };
-
+export function BookGrid({ books, addToCart }) {
   return (
     <section className="grid-container">
       {books.map((book) => {
         return (
           <BookCard
+            id={book.id}
+            key={book.id}
             addToCart={addToCart}
             book={book}
-            key={book.id}
             image={book.coverImage}
             category={book.primaryCollection}
             title={book.title}

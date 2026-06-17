75345f637ca18924a3f4657efc157f037b396904
kingsley <kingsleykay26@gmail.com>
2026-06-17 11:26:44 +0100

    chore: set up router and lint helpers

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,15 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
+import reactPlugin from "eslint-plugin-react";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{js,jsx}"],
+    plugins: {
+      react: reactPlugin,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -23,7 +27,15 @@ export default defineConfig([
       },
     },
     rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "no-unused-vars": [
+        "warn",
+        { vars: "all", args: "after-used", ignoreRestSiblings: true },
+      ],
+      "no-undef": "warn",
+      "no-console": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
     },
   },
 ]);

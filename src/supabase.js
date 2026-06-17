8c5b4f4320fe950a825f00e3d5ca7ce7ac5575ff
kingsley <kingsleykay26@gmail.com>
2026-06-17 11:28:35 +0100

    chore: remove unused legacy files

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://wzlttutwnbflmurdvzzj.supabase.co";
-const supabaseKey = "sb_publishable_Cl3GQIK4nNkgDqVTduja6w_-LU6H1ZN";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);

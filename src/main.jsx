01c46ce919ae99d2e1f44bd159e841713cca889c
kingsley <kingsleykay26@gmail.com>
2026-07-18 19:35:23 +0100

    chore(ui): add Sonner toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -8,6 +9,17 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster
+        position="top-right"
+        richColors
+        duration={3000}
+        toastOptions={{
+          style: {
+            borderRadius: "0px",
+            shadow: "0 8px 20px rgba(0, 0, 0, 0.08)",
+          },
+        }}
+      />
     </BrowserRouter>
   </StrictMode>,
 );

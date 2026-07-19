2b3b97ebff8827d2716ec442a7e032314524fc6b
kingsley <kingsleykay26@gmail.com>
2026-07-19 16:29:18 +0100

    feat(account): add account drawer variants for guest and authenticated users

---
+++ src/feature/account/AuthenticatedAccountDrawer.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+import { BsChevronRight } from "react-icons/bs";
+import { MdOutlineClose } from "react-icons/md";
+
+export function AuthenticatedAccountDrawer({ isOpen, onClose, user, email }) {
+  const accountNav = [
+    { title: "Orders", desc: "Track and view your orders", link: "" },
+    { title: "Addresses", desc: "Manage your saved addreses", link: "" },
+    {
+      title: "Account Settings",
+      desc: "Update your personal information",
+      link: "",
+    },
+  ];
+  return (
+    <Drawer isOpen={isOpen} onClose={onClose}>
+      <div className="account-drawer-wrapper">
+        <header>
+          <div className="account-drawer-header-n-close">
+            <h1>Hi, {user}</h1>
+            <button onClick={onClose} className="close-account-drawer">
+              <MdOutlineClose />
+            </button>
+          </div>
+          <p className="account-drawer-subtext">{email}</p>
+
+          <div className="account-drawer-buttons-wrapper">
+            <button className="button-primary">Sign In</button>
+            <p className="new-here-login">
+              New here?{" "}
+              <Link className="account-drawer-link" to="">
+                Create an account
+              </Link>
+            </p>
+          </div>
+        </header>
+        <nav>
+          <ul>
+            {accountNav.map((nav) => (
+              <li>
+                <div>
+                  <p>{nav.title}</p>
+                  <p className="account-nav-subtext">{nav.desc}</p>
+                </div>
+                <BsChevronRight />
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <footer className="account-drawer-footer">
+          <button className="button-secondary">Sign Out</button>
+        </footer>
+      </div>
+    </Drawer>
+  );
+}
---
+++ src/feature/account/GuestAccountDrawer.jsx
@@ -0,0 +1,57 @@
+import Drawer from "../../component/components/Drawer/drawer";
+import { Link } from "react-router-dom";
+import { BsChevronRight } from "react-icons/bs";
+import { MdOutlineClose } from "react-icons/md";
+
+export function GuestAccountDrawer({ isOpen, onClose }) {
+  const accountNav = [
+    { title: "Orders", desc: "Track and view your orders", link: "" },
+    { title: "Addresses", desc: "Manage your saved addreses", link: "" },
+    {
+      title: "Account Settings",
+      desc: "Manage your account after signing in",
+      link: "",
+    },
+  ];
+
+  return (
+    <Drawer isOpen={isOpen} onClose={onClose}>
+      <div className="account-drawer-wrapper">
+        <header>
+          <div className="account-drawer-header-n-close">
+            <h1>My Account</h1>
+            <button onClick={onClose} className="close-account-drawer">
+              <MdOutlineClose />
+            </button>
+          </div>
+          <p className="account-drawer-subtext">
+            Sign in to track orders,and save your details
+          </p>
+
+          <div className="account-drawer-buttons-wrapper">
+            <button className="button-primary">Sign In</button>
+            <p className="new-here-login">
+              New here?{" "}
+              <Link className="account-drawer-link" to="">
+                Create an account
+              </Link>
+            </p>
+          </div>
+        </header>
+        <nav>
+          <ul>
+            {accountNav.map((nav) => (
+              <li>
+                <div>
+                  <p>{nav.title}</p>
+                  <p className="account-nav-subtext">{nav.desc}</p>
+                </div>
+                <BsChevronRight />
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </Drawer>
+  );
+}

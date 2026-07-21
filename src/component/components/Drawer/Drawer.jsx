0829c8520077bac9e084c05c42bf22bb61cd3faf
kingsley <kingsleykay26@gmail.com>
2026-07-21 13:10:34 +0100

    refactor(drawer): remove unused code

--- src/component/components/Drawer/Drawer.jsx
+++ src/component/components/Drawer/Drawer.jsx
@@ -1,8 +1,4 @@
 import "./Drawer.css";
-// Drawer.Header = DrawerHeader;
-// Drawer.Body = DrawerBody;
-// Drawer.Footer = DrawerFooter;
-
 function Drawer({ isOpen, onClose, position = "right", children }) {
   return (
     <>
@@ -18,17 +14,5 @@ function Drawer({ isOpen, onClose, position = "right", children }) {
     </>
   );
 }
-
-// function DrawerHeader({ children }) {
-//   return <header className="drawer__header">{children}</header>;
-// }
-
-// function DrawerBody({ children }) {
-//   return <div className="drawer__body">{children}</div>;
-// }
-
-// function DrawerFooter({ children }) {
-//   return <footer className="drawer__footer">{children}</footer>;
-// }
 
 export default Drawer;

74ebb56662b77c1ee655b75b281f30b60c16f4f0
kingsley <kingsleykay26@gmail.com>
2026-07-28 14:12:57 +0100

    feat(product): create recommended books component

---
+++ src/component/product/BookRecommendationGrid.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { books } from "../../data/inventory";
+import { BookCardRecommendationCard } from "../BookCardRecommendationCard";
+import "./BookRecommendationGrid.css";
+
+const BookRecommendationGrid = ({ id, selectedBook }) => {
+  const collections = selectedBook.collections;
+  const recommendedBooks = books
+    .filter((book) => book.id !== id)
+    .map((book) => ({
+      ...book,
+      score: book.collections.filter((collection) =>
+        collections.includes(collection),
+      ).length,
+    }))
+    .filter((book) => book.score > 0)
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 12);
+
+  return (
+    <div className="related-reads">
+      <article className="recommended-book-container special-days">
+        {recommendedBooks.map((book, index) => (
+          <BookCardRecommendationCard key={index} book={book} />
+        ))}
+      </article>
+    </div>
+  );
+};
+
+export default BookRecommendationGrid;

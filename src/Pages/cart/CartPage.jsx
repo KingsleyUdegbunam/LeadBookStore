c7244ce125f078500eb813a302c3ae87cae1379b
kingsley <kingsleykay26@gmail.com>
2026-08-06 13:43:45 +0100

    feat(cart): add empty state

--- src/Pages/cart/CartPage.jsx
+++ src/Pages/cart/CartPage.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { RiArrowLeftLongLine } from "react-icons/ri";
 import { useCart } from "../../context/CartContext";
-import { FaLock } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 import { CartItemsTable } from "../../feature/cart/components/CartItemsTable";
 import { CartCostSummary } from "../../feature/cart/components/CartCostSummary";
+import { EmptyState } from "../../component/EmptyState";
+import { EMPTY_STATES } from "../../constants/emptyStatesCopies";
+import { FaLock } from "react-icons/fa";
+import { RiArrowLeftLongLine } from "react-icons/ri";
 import "./CartPage.css";
 
 export default function CartPage() {
   const [qtyInputs, setQtyInputs] = useState({});
   const { cart } = useCart();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const initial = cart.reduce((acc, cartItem) => {
@@ -30,27 +33,43 @@ export default function CartPage() {
                 Shopping Cart{" "}
                 <span className="small-nav-item-count">{`(${cart.length})`}</span>
               </h2>
-              <Link to="/shop" className="navigation">
-                <RiArrowLeftLongLine />
-                <span className="">Continue Shopping</span>
+              {cart.length > 0 && (
+                <Link to="/shop" className="navigation">
+                  <RiArrowLeftLongLine />
+                  <span className="">Continue Shopping</span>
+                </Link>
+              )}
+            </div>
+          </article>
+          {cart.length > 0 ? (
+            <div>
+              <CartItemsTable
+                qtyInputs={qtyInputs}
+                setQtyInputs={setQtyInputs}
+              />
+              <CartCostSummary />
+              <Link
+                className="button-primary button-full-width checkout-button"
+                to="/checkout"
+              >
+                <FaLock />
+                Proceed to Checkout
+              </Link>
+              <Link
+                className="button-full-width checkout-back-to-shop-btn"
+                to="/shop"
+              >
+                Continue Shopping
               </Link>
             </div>
-            <CartItemsTable qtyInputs={qtyInputs} setQtyInputs={setQtyInputs} />
-          </article>
-          <CartCostSummary />
-          <Link
-            className="button-primary button-full-width checkout-button"
-            to="/checkout"
-          >
-            <FaLock />
-            Proceed to Checkout
-          </Link>
-          <Link
-            className="button-full-width checkout-back-to-shop-btn"
-            to="/shop"
-          >
-            Continue Shopping
-          </Link>
+          ) : (
+            <EmptyState
+              {...EMPTY_STATES.cart}
+              onAction={() => {
+                navigate("/shop");
+              }}
+            />
+          )}
         </article>
       </section>
     </>

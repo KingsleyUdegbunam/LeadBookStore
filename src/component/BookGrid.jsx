941824aa597346c9cc59321531770267e1b5be23
kingsley <kingsleykay26@gmail.com>
2026-08-15 16:28:16 +0100

    refactor(shop): update props passed to BookCard

--- src/component/BookGrid.jsx
+++ src/component/BookGrid.jsx
@@ -1,22 +1,11 @@
 import { BookCard } from "./BookCard";
 import "./BookGrid.css";
 
-export function BookGrid({ books, addToCart }) {
+export function BookGrid({ books }) {
   return (
     <section className="grid-container">
       {books.map((book) => {
-        return (
-          <BookCard
-            id={book.id}
-            key={book.id}
-            addToCart={addToCart}
-            book={book}
-            image={book.coverImage}
-            category={book.primaryCollection}
-            title={book.title}
-            priceInKobo={book.price.paperback}
-          />
-        );
+        return <BookCard book={book} />;
       })}
     </section>
   );

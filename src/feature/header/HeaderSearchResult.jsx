f4e8619f1145564edb5725673c0c2963da75e2bf
kingsley <kingsleykay26@gmail.com>
2026-08-25 20:33:32 +0100

    refactor(header): replace SVG with react icon

--- src/feature/header/HeaderSearchResult.jsx
+++ src/feature/header/HeaderSearchResult.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { convertToNaira } from "../../utilities/money";
-
-import { MdOutlineClose } from "react-icons/md";
 import { books } from "../../data/inventory";
+import { convertToNaira } from "../../utilities/money";
 import { EmptyState } from "../../component/general/states/EmptyState";
 import { EMPTY_STATES } from "../../constants/emptyStatesCopies";
+import { ImSearch } from "react-icons/im";
+import { MdOutlineClose } from "react-icons/md";
 import "./HeaderSearchResult.css";
 
 export const HeaderSearchResult = ({
@@ -52,16 +51,7 @@ export const HeaderSearchResult = ({
             id="search-input-field"
           />
           <div className="overlay-placeholder-btn">
-            <svg
-              className="icon search-icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-            </svg>
+            <ImSearch size={16} />
           </div>
         </label>
       </div>
